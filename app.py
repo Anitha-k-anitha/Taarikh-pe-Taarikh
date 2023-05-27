6642329425719003a7276a8ef982bddc641fab5e
Mihika Gaonkar <mihika831@gmail.com>
2023-05-27 16:51:08 +0530

    :tada: feat: Create app

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, request, flash, url_for, redirect, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///students.sqlite3'
+app.config['SECRET_KEY'] = "random string"
+
+db = SQLAlchemy(app)
+
+
+if __name__ == '__main__':
+   app.run(debug = True)
